69df1df6c5104cfab7684d3fe0c595971b1f7b0c
Nakul Bende <nakulbende@gmail.com>
2018-06-03 17:39:55 -0400

    Add files via upload

---
+++ IR_read_RAW_codes/IR_read_RAW_codes.ino
@@ -0,0 +1,24 @@
+#include "IRLibAll.h"
+ 
+//Create a receiver object to listen on pin 2
+IRrecvPCI myReceiver(2);
+ 
+//Create a decoder object 
+IRdecode myDecoder;   
+ 
+void setup() {
+  Serial.begin(9600);
+  delay(2000); while (!Serial); //delay for Leonardo
+  myReceiver.enableIRIn(); // Start the receiver
+  Serial.println(F("Ready to receive IR signals"));
+}
+ 
+void loop() {
+  //Continue looping until you get a complete signal received
+  if (myReceiver.getResults()) {
+    myDecoder.decode();           //Decode it
+    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    myReceiver.enableIRIn();      //Restart receiver
+  }
+}
+
